438282ea519c7e428218d0cebe66ac5ca0c26a66
hariom575 <mewadahariom520@gmail.com>
2026-02-06 12:08:34 +0530

    does capitalism totally corrupted human soul?

---
+++ AnalysisTransformer.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import soot.*;
+import soot.jimple.AnyNewExpr;
+import soot.jimple.Ref;
+import soot.jimple.internal.JAssignmentStmt;
+import soot.jimple.internal.JnewExpr;
+import soot.toolkits.graph.*;
+import soot.toolkits.scalar.BackWardFlowAnalysis;
+import soot.toolkits.scalar.FlowSet;
+public class AnalysisTransformer extends BodyTransformer{
+    protected void internalTransform(Body body, String phaseName,,Map<String,String> options){
+        
+    }
+}
---
+++ testcases/Test1/Test.java
@@ -0,0 +1,18 @@
+class Node{
+    Node f1;
+    Node f2;
+    Node g;
+    Node(){}
+}
+public class Test{
+     public static void main(String [] args){
+        Node a = new Node();
+        a.f1 = new Node();
+        Node b = new Node();
+        b.f1 = new Node();
+        a.f2 = new Node();
+        Node c = a1.f1;
+        a.f2 = a.f1;
+        b.f1 = a.f2;
+     }
+}
